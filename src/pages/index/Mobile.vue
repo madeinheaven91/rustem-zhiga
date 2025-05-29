9df00d2b46cb659371273d08425324aa3c6ef9e1
madeinheaven91 <exspectanss@gmail.com>
2025-05-29 21:55:46 +0300

    index page almost done

--- src/pages/index/Mobile.vue
+++ src/pages/index/Mobile.vue
@@ -1,44 +1,46 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import Sidebar from '@/widgets/sidebar/Sidebar.vue';
 
+const width = ref("");
 onMounted(() => {
-
+    let img = document.getElementById("image")
+    width.value = img?.clientHeight as number + "px";
 })
 
 </script>
 
 <template>
-    <div class="wrapper h-screen w-screen">
+    <div class="relative overflow-hidden h-screen w-screen">
         <Sidebar />
+        <img id="image" class="h-screen" src="@/assets/rustem.jpg">
         <div class="pt-80 title">
             <div class="w-50 h-px bg-white" />
             <span class="text-6xl great-vibes mt-5">Рустем Жига</span>
-            <span class="text-2xl text-gray-200 caveat-400">Музыка для души</span>
+            <span class="text-2xl caveat-400">Музыка для души</span>
         </div>
     </div>
-    <footer class="h-12 text-white">madeinheaven91</footer>
+    <footer class="p-2 text-gray-300 text-center">Сделано студией канал шопенгауэра</footer>
 </template>
 
 <style lang="css" scoped>
-.wrapper {
-    background-image: url(@/assets/rustem.jpg);
-    background-repeat: no-repeat;
-    background-size: 100vh;
-        animation: images 30s ease-in-out infinite;
+#image {
+    @apply absolute;
+    width: v-bind(width);
+    max-width: v-bind(width);
+    animation: images 60s;
+    transform: translateX(-100px);
+    filter: brightness(0.8);
 }
 
 @keyframes images {
-    0% {
-        background-position-x: -100px;
-    }
-    50% {
-        background-position-x: -50px;
-    }
+    0% {}
+
     100% {
-        background-position-x: -100px;
+        scale: 130%;
     }
 }
+
 .overlay {
     background: rgba(255, 255, 255, 1);
     background: radial-gradient(circle, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.9) 50%, rgba(0, 0, 0, 1) 100%);
